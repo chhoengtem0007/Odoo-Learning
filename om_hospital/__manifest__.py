8edbe712950f21f82418bbc280cb111fe0e2a6a6
Tem CHHOENG <temchhoeng@ramekhchhoeng.local>
2025-06-07 15:27:55 +0700

    update

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : 'Hospital Management',
+    'version' : '18.0.1.0.0',
+    'category' :'Hospital',
+    'author' : 'CHHOENG TEM',
+    'sequence' : -70,
+    'summary' : 'This is hospital management system',
+    'discription' : 'This is hospital management system',
+    'depends' : [],
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demeo' : [],
+    'application' : True,
+    'auto_install' : False,
+    'asset' : {},
+
+}
