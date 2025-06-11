afc3d2a2af7de562180010ca58b86ee0e04c6cf7
chhoengtem <temchhoeng009@gmail.com>
2025-06-11 17:56:19 +0700

    Update on 11-jun-2025

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,9 +1,17 @@
 from odoo import api, fields, models
 
+
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = "Hospital Patient"
 
-    name = fields.Char(string = 'Name')
-    age = fields.Integer(string = 'Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string = 'Gender')
+
+    ref = fields.Char(string="Reference")
+    name = fields.Char(string='Name')
+    phone = fields.Char(string='Phone Number')
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
+    age_type = fields.Selection([
+        ('old', "Old"),
+        ('kid', "Kid"),
+    ], string="Age Type")
